2bad4602b0f713c939a56b043e46d355f9f51e80
Tony Heupel <tonyheupel@gmail.com>
2012-01-07 21:13:20 -0800

    simplify partial implementation to only need Partial to be exported

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var Liquid = require('liquid-node')
-
-Liquid.Tempates = require('./lib/templates')
-
-
-module.exports = Liquid.Partial = require('./lib/partial')
+module.exports = require('./lib/partial')
--- test/partial-test.js
+++ test/partial-test.js
@@ -2,27 +2,27 @@ var vows   = require('vows'),
     assert = require('assert');
 
 Liquid = require('liquid-node');
-Liquid.Partial = require('../lib/partial');
+Liquid.Partial = require('../index');
 
 vows.describe('Partial Tag').addBatch({
   'Register a template': {
     topic: 'My template',
     "should convert a string to a Template": function(topic) {
-      Liquid.registerTemplate('foobar', topic);
-      assert.instanceOf(Liquid.Templates['foobar'], Liquid.Template);
+      Liquid.Partial.registerTemplate('foobar', topic);
+      assert.instanceOf(Liquid.Partial.Templates['foobar'], Liquid.Template);
     },
     "should simply use a Template as-is": function(topic) {
       var template = Liquid.Template.parse(topic);
-      Liquid.registerTemplate('bizbuzz', template);
-      assert.strictEqual(Liquid.Templates['bizbuzz'], template);
+      Liquid.Partial.registerTemplate('bizbuzz', template);
+      assert.strictEqual(Liquid.Partial.Templates['bizbuzz'], template);
     }
   },
   'Render a valid partial template': {
     topic: 'This template uses foobar value and makes it uppercase: {{ foobar | upcase }}',
 
     "should insert the partial's content and do proper replacements": function (topic) {
-			var template = Liquid.Template.parse("Before partial\n{% partial sample-partial %}\nAfter partial");
-			Liquid.registerTemplate('sample-partial', topic);
+      var template = Liquid.Template.parse("Before partial\n{% partial sample-partial %}\nAfter partial");
+      Liquid.Partial.registerTemplate('sample-partial', topic);
 
       var actual = template.renderOrRaise({ foobar: 'The foobar value' });
       assert.equal(actual, "Before partial\nThis template uses foobar value and makes it uppercase: THE FOOBAR VALUE\nAfter partial");
